c319c9348b6f43d683f9be745844a23a4cca05dc
Mike Harris <MikeMKH@users.noreply.github.com>
2019-08-20 06:15:11 -0500

    add examples from chapter 7.4 - 7.5

--- ch07/examples.R
+++ ch07/examples.R
@@ -149,6 +149,7 @@ cor.test(states[,2], states[,6])
 library(psych)
 corr.test(states, use="complete")
 
+library(psych)
 library(quantmod)
 
 # CPI, Swiss, Norway, Sweden, UK, 5000
@@ -167,3 +168,37 @@ summary(ret)
 corr.test(ret, use="complete")
 print(corr.test(ret, use="complete"), short=FALSE)
 # US/CH and US/GB exchange rate are nearly independent
+
+with(ret, t.test(coredata(AEXUSUK), coredata(AEXSZUS)))
+with(ret, t.test(coredata(AEXUSUK), coredata(AEXSZUS), paired=TRUE))
+
+library(MASS)
+summary(UScrime)
+pairs(UScrime)
+
+# t test
+t.test(Prob ~ So, data=UScrime)
+
+# dependent t test
+sapply(UScrime[c("U1","U2")], function(x)(c(mean=mean(x),sd=sd(x))))
+with(UScrime, t.test(U1, U2, paired=TRUE))
+
+# Wilcoxon two group comparison
+with(UScrime, by(Prob, So, median))
+wilcox.test(Prob ~ So, data=UScrime)
+
+sapply(UScrime[c("U1", "U2")], median)
+with(UScrime, wilcox.test(U1, U2, paired=TRUE))
+
+# Kruskal Wallis test
+states <- data.frame(state.region, state.x77)
+kruskal.test(Illiteracy ~ state.region, data=states)
+
+# 7.17
+source ("http://www.statmethods.net/RiA/wmc.txt")
+states <- data.frame(state.region, state.x77)
+summary(states)
+head(states)
+
+# Nonparametric multiple comparisons
+wmc(Illiteracy ~ state.region, data=states, method="holm")
