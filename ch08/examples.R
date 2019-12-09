7d97dbc8d9ec8834f50f49cd53b84f8c211ab945
Mike Harris <MikeMKH@users.noreply.github.com>
2019-12-09 06:36:51 -0600

    add examples from chapter 8.3

--- ch08/examples.R
+++ ch08/examples.R
@@ -48,3 +48,73 @@ summary(fit)
 
 library(effects)
 plot(effect("hp:wt", fit,, list(wt=c(2.2,3.2,4.2))), multiline=TRUE)
+
+# 8.3
+states <- as.data.frame(
+  state.x77[,c("Murder", "Population", "Illiteracy", "Income", "Frost")])
+fit <- lm(Murder~Population+Illiteracy+Income+Frost, data=states)
+summary(fit)
+confint(fit)
+
+fit <- lm(weight~height, data=women)
+par(mfrow=c(2,2))
+plot(fit)
+
+fit2 <- lm(weight~height+I(height^2), data=women)
+plot(fit2)
+
+fit3 <- lm(weight~height+I(height^2)+I(height^3), data=women)
+plot(fit3)
+
+newfit2 <- lm(weight~height+I(height^2), data=women[-c(13,14,15),])
+plot(newfit2)
+
+newfit3 <- lm(weight~height+I(height^2)+I(height^3), data=women[-c(13,14,15),])
+plot(newfit3)
+
+library(car)
+states <- as.data.frame(
+  state.x77[,c("Murder", "Population", "Illiteracy", "Income", "Frost")])
+fit <- lm(Murder~Population+Illiteracy+Income+Frost, data=states)
+
+par(mfrow=c(1,1))
+qqPlot(fit, labels=row.names(states),
+       id.method="identify", simulate=TRUE, main="Q-Q Plot")
+
+states["Nevada",]
+fitted(fit)["Nevada"]
+residuals(fit)["Nevada"]
+rstudent(fit)["Nevada"]
+
+residplot <- function(fit, nbreaks=10) {
+  z <- rstudent(fit)
+  hist(z, breaks=nbreaks, freq=FALSE,
+       xlab="Studentized Residual",
+       main="Distribution of Errors")
+  rug(jitter(z), col="brown")
+  curve(dnorm(x, mean=mean(z), sd=sd(z)),
+        add=TRUE, col="blue", lwd=2)
+  lines(density(z)$x, density(z)$y,
+        col="red", lwd=2, lty=2)
+  legend("topright",
+         legend = c( "Normal Curve", "Kernel Density Curve"),
+         lty=1:2, col=c("blue","red"), cex=.7)
+}
+
+residplot(fit)
+
+durbinWatsonTest(fit)
+durbinWatsonTest(fit,simulate=FALSE)
+
+crPlots(fit)
+
+ncvTest(fit)
+
+spreadLevelPlot(fit)
+
+library(gvlma)
+model <- gvlma(fit)
+summary(model)
+
+vif(fit)
+sqrt(vif(fit)) > 2
