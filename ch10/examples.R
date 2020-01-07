cd2d4491f7788770943a70144d65aca149fb7810
Mike Harris <MikeMKH@users.noreply.github.com>
2020-01-07 06:07:29 -0600

    add examples from chapter 10.3

--- ch10/examples.R
+++ ch10/examples.R
@@ -30,3 +30,41 @@ plot(samsize,es, type="l", lwd=2, col="red",
      ylab="Effect Size",
      xlab="Sample Size (per cell)",
      main="One Way ANOVA with Power=.90 and Alpha=.05")
+
+# 10.3
+library(pwr)
+
+r <- seq(0.1, 0.5, 0.01)
+nr <- length(r)
+
+p <- seq(0.4, 0.9, 0.1)
+np <-length(p)
+
+samsize <- array(numeric(nr*np), dim=c(nr,np))
+for (i in 1:np) {
+  for (j in 1:nr) {
+    result <- pwr.r.test(
+      n=NULL, r=r[j], sig.level=0.05, power=p[i],
+      alternative="two.sided")
+    samsize[j, i] <- ceiling(result$n)
+  }
+}
+summary(samsize)
+
+xrange <- range(r)
+yrange <- round(range(samsize))
+colors <- rainbow(np)
+
+plot(xrange, yrange, type="n",
+     xlab="Correlation Coefficient (r)",
+     ylab="Sample Size (n)")
+
+for (i in 1:np) {
+  lines(r, samsize[,i], type="l", lwd=2, col=colors[i])
+}
+
+abline(v=0, h=seq(0,yrange[2],50), lty=2, col="grey89")
+abline(h=0, v=seq(xrange[1],xrange[2],0.02), lty=2, col="grey89")
+
+title("Sample Size Estimation for Correlation Studies\nSig=0.05 (Two-tailed)")
+legend("topright", title="Power", as.character(p), fill=colors)
