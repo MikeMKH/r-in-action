c52daec2408a0564e78ce5cf86b41d6e7b4872e2
Mike Harris <MikeMKH@users.noreply.github.com>
2020-01-23 06:32:40 -0600

    add examples from chapter 14.3

--- ch14/examples.R
+++ ch14/examples.R
@@ -29,3 +29,43 @@ cor(USJudgeRatings$CONT, pc$scores)
 library(psych)
 rc <- principal(Harman23.cor$cov, nfactors=2, rotate="varimax")
 round(unclass(rc$weights), 2)
+
+# 14.3
+library(psych)
+(cv <- ability.cov$cov)
+(cr <- cov2cor(cv))
+fa.parallel(cr, n.obs=12, fa="both",
+            n.iter=100, main="Scree plots with parallel analysis")
+(fa <- fa(cr, nfactors=2, rotate="none", fm="pa"))
+
+(fa.varimax <- fa(cr, nfactors=2, rotate="varimax", fm="pa"))
+(fa.promax <- fa(cr, nfactors=2, rotate="promax", fm="pa"))
+
+fsm <- function(oblique) {
+  if (class(oblique)[2]=="fa" & is.null(oblique$Phi)) {
+    warning("Object doesn't look like oblique EFA")
+  } else {
+    P <- unclass(oblique$loading)
+    F <- P %*% oblique$Phi
+    colnames(F) <- c("PA1", "PA2")
+    return(F)
+  }
+}
+
+fsm(fa.promax)
+
+factor.plot(fa.varimax, labels=rownames(fa.promax$loadings))
+factor.plot(fa.promax, labels=rownames(fa.promax$loadings))
+
+fa.diagram(fa.varimax, simple=FALSE)
+fa.diagram(fa.promax, simple=FALSE)
+
+fa.varimax$weights
+fa.promax$weights
+
+library(psych)
+(fa.24tests <- fa(Harman74.cor$cov, nfactors=4, rotate="promax"))
+factor.plot(fa.24tests, labels=rownames(fa.24tests$loadings))
+fa.diagram(fa.24tests, simple=TRUE)
+fa.diagram(fa.24tests, simple=FALSE)
+fa.24tests$weights
