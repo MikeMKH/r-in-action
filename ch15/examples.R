f6f4d7b5073928573c0817fe47d2c404a236d9e5
Mike Harris <MikeMKH@users.noreply.github.com>
2020-01-27 06:22:06 -0600

    add examples from chapter 15.2

--- ch15/examples.R
+++ ch15/examples.R
@@ -7,3 +7,33 @@ start(tsales)
 frequency(tsales)
 
 (tsales.subset <- window(tsales, start=c(2018, 5), end=c(2019, 6)))
+
+# 15.2
+library(forecast)
+opar <- par(no.readonly=TRUE)
+par(mfrow=c(2,2))
+ylim <- c(min(Nile), max(Nile))
+plot(Nile, main="Raw time series")
+plot(ma(Nile, 3), main="Simple Moving Averages (k=3)", ylim=ylim)
+plot(ma(Nile, 7), main="Simple Moving Averages (k=7)", ylim=ylim)
+plot(ma(Nile, 15), main="Simple Moving Averages (k=15)", ylim=ylim)
+par(opar)
+
+summary(AirPassengers)
+plot(AirPassengers)
+
+data <- log(AirPassengers)
+summary(data)
+fit <- stl(data, s.window="period")
+summary(fit)
+plot(fit)
+
+fit$time.series
+exp(fit$time.series)
+
+opar <- par(no.readonly=TRUE)
+library(forecast)
+par(mfrow=c(2,1))
+monthplot(AirPassengers, xlab="",  ylab="")
+seasonplot(AirPassengers, year.labels="TRUE", main="")
+par(opar)
