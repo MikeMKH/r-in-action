4d9b68ac086bc7ce0ca4f25fbed578cbcea098f5
Mike Harris <MikeMKH@users.noreply.github.com>
2020-02-03 06:24:13 -0600

    add examples from chapter 16.4

--- ch16/examples.R
+++ ch16/examples.R
@@ -71,3 +71,48 @@ table(clusters)
 aggregate(nutrient, by=list(clusters=clusters), median)
 aggregate(as.data.frame(nutrient.scaled), by=list(clusters=clusters), median)
 plot(fit.complete, hang=-1, cex=.8, main="Complete Linkage\n5 Cluster")
+
+# 16.4
+wssplot <- function(data, nc=15, seed=1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc){
+    set.seed(seed)
+    wss[i] <- sum(kmeans(data, centers=i)$withinss)}
+  plot(1:nc, wss, type="b", xlab="Number of Clusters",
+       ylab="Within groups sum of squares")}
+
+data(wine, package="rattle")
+head(wine)
+pairs(wine)
+
+df <- scale(wine[-1])
+wssplot(df)
+
+library(NbClust)
+set.seed(1234)
+devAskNewPage(ask=FALSE)
+nc <- NbClust(df, min.nc=2, max.nc=15, method="kmeans")
+n <- nc$Best.n[1,]
+table(n)
+
+par(mfrow=c(1,1))
+barplot(table(n), xlab="Number of Clusters", ylab="Number of Criteria",
+        main="Number of Clusters Chosen by Criteria")
+
+set.seed(1234)
+fit.kmeans <- kmeans(df, 3, nstart=25)
+fit.kmeans$size
+fit.kmeans$centers
+aggregate(wine[-1], by=list(clusters=fit.kmeans$cluster), mean)
+
+(ct.kmeans <- table(wine$Type, fit.kmeans$cluster))
+library(flexclust)
+randIndex(ct.kmeans)
+
+library(cluster)
+set.seed(1234)
+fit.pam <- pam(wine[-1], k=3, stand=TRUE)
+fit.pam$medoids
+clusplot(fit.pam, main="Bivariate Cluster Plot")
+(ct.pam <- table(wine$Type, fit.pam$clustering))
+randIndex(ct.pam)
