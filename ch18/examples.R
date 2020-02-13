beb2728e2d15d4451659846cf38694c4b52a2f92
Mike Harris <MikeMKH@users.noreply.github.com>
2020-02-13 06:18:20 -0600

    add examples from chapter 18.3

--- ch18/examples.R
+++ ch18/examples.R
@@ -7,3 +7,26 @@ sleep[!complete.cases(sleep),]
 sum(is.na(sleep$Dream))
 mean(is.na(sleep$Dream))
 mean(!complete.cases(sleep))
+
+# 18.3
+library(mice)
+data(sleep, package="VIM")
+summary(sleep)
+
+md.pairs(sleep)
+md.pattern(sleep)
+
+library(VIM)
+aggr(sleep, prop=FALSE, numbers=TRUE)
+matrixplot(sleep)
+marginplot(sleep[c("Gest", "Dream")],
+           pch=c(20), col=c("blue", "red", "black"))
+
+x <- as.data.frame(abs(is.na(sleep)))
+head(sleep, n=5)
+head(x, n=5)
+
+y <- x[which(apply(x, 2, sum)>0)]
+head(y, n=5)
+cor(y)
+cor(sleep, y, use="pairwise.complete.obs")
